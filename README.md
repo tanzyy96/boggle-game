c343b802031127f5c2b0473917afa9165d7315b8
Tan Zhi Yang <tan.zy096@gmail.com>
2020-03-08 21:15:21 +0800

    Removed temp files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-flask-proj
+BOGGLE
--- page.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
-
-@app.route('/')
-def welcome():
-    return render_template("main.html")
